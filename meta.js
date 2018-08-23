c95c8c96fe43d16d3ed6b7ad46f050f7fbea1ad1
wangqiang <wangqiang@ddjf.com.cn>
2018-08-23 17:52:23 +0800

    添加基本模板

---
+++ meta.js
@@ -0,0 +1,83 @@
+const path = require('path')
+const fs = require('fs')
+
+const {
+  sortDependencies,
+  installDependencies,
+  runLintFix,
+  printMessage,
+} = require('./utils')
+const pkg = require('./package.json')
+
+const templateVersion = pkg.version
+
+const { addTestAnswers } = require('./scenarios')
+
+module.exports = {
+  metalsmith: {
+    // When running tests for the template, this adds answers for the selected scenario
+    before: addTestAnswers
+  },
+  helpers: {
+    if_or(v1, v2, options) {
+
+      if (v1 || v2) {
+        return options.fn(this)
+      }
+
+      return options.inverse(this)
+    },
+    template_version() {
+      return templateVersion
+    },
+  },
+  
+  prompts: {
+    name: {
+      when: 'isNotTest',
+      type: 'string',
+      required: true,
+      message: 'Project name',
+    },
+    description: {
+      when: 'isNotTest',
+      type: 'string',
+      required: false,
+      needTip: false,
+      message: 'Project description',
+      default: 'A component project for publish component',
+    },
+    author: {
+      when: 'isNotTest',
+      type: 'string',
+      message: 'Author',
+    }
+  },
+  filters: {
+    'test/**/*': 'test',
+    'store/**/*': 'vuex',
+    'route/**/*': 'router'
+  },
+  complete: function(data, { chalk }) {
+    const green = chalk.green
+
+    sortDependencies(data, green)
+
+    const cwd = path.join(process.cwd(), data.inPlace ? '' : data.destDirName)
+
+    if (data.autoInstall) {
+      installDependencies(cwd, data.autoInstall, green)
+        .then(() => {
+          return runLintFix(cwd, data, green)
+        })
+        .then(() => {
+          printMessage(data, green)
+        })
+        .catch(e => {
+          console.log(chalk.red('Error:'), e)
+        })
+    } else {
+      printMessage(data, chalk)
+    }
+  },
+}
