57c969f27090db8737e13034a3f5cba292ff3bf4
wangqiang <wangqiang@ddjf.com.cn>
2018-08-31 15:41:20 +0800

    修改模板

--- template/src/main.js
+++ template/src/main.js
@@ -2,15 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import ElementUI from 'element-ui'
 import './css/_normalize.scss'
 import './css/main.scss'
-
+{{#router}}
+import router form './route'
+{{/router}}
 Vue.config.productionTip = false
-Vue.use(ElementUI);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  {{#router}}
+  router,
+  {{/router}}
   components: { App },
   template: '<App/>'
 })
